f653f53a4d203d2e2d4bf34b1765ee4bd59f798d
Kerem Tezcan <kctezcan@Kerems-MacBook-Pro.local>
2024-12-10 14:36:25 +0100

    3 iamges

--- app.py
+++ app.py
@@ -33,22 +33,57 @@ import tempfile  # Import tempfile for temporary file handling
 
 st.title("NIFTI Image Middle Slice Viewer")
 
-input_path = st.file_uploader('Upload files')
+input_path_img = st.file_uploader('Upload image files')
+input_path_segm = st.file_uploader('Upload segmentation files')
+input_path_ts = st.file_uploader('Upload TS files')
 
+if (not input_path_img) or (not input_path_segm) or (not input_path_ts):
+    st.write("upload all files")
 
-if input_path:
+col1, col2, col3 = st.columns(3)
+
+if input_path_img and input_path_segm and input_path_ts:
     temp_data_directory = './data/temp_folder/'
     os.makedirs(temp_data_directory, exist_ok=True)
 
-    with open(temp_data_directory + "niftifile.nii", 'wb') as out:
-            out.write(input_path.getbuffer())
+    with open(temp_data_directory + "niftifile_img.nii", 'wb') as out:
+            out.write(input_path_img.getbuffer())
+
+    with open(temp_data_directory + "niftifile_segm.nii", 'wb') as out:
+            out.write(input_path_segm.getbuffer())
+
+    with open(temp_data_directory + "niftifile_ts.nii", 'wb') as out:
+            out.write(input_path_ts.getbuffer())
 
 
-    img = nib.load(temp_data_directory + "niftifile.nii")
+    img_img = nib.load(temp_data_directory + "niftifile_img.nii")
+    img_segm = nib.load(temp_data_directory + "niftifile_segm.nii")
+    img_ts = nib.load(temp_data_directory + "niftifile_ts.nii")
 
     # Get the image data as a NumPy array
-    data = img.get_fdata()
+    data_img = img_img.get_fdata()
+    data_segm = img_segm.get_fdata()
+    data_ts = img_ts.get_fdata()
 
-    st.image(data[:,:,200]/np.max(data[:,:,200]))
+    data_img = np.transpose(data_img, [0, 2,1 ])
+    data_segm = np.transpose(data_segm, [0, 2,1 ])
+    data_ts = np.transpose(data_ts, [0, 2,1 ])
+
+    data_img = np.rot90( data_img, 1)
+    data_segm = np.rot90( data_segm, 1)
+    data_ts = np.rot90( data_ts, 1)
+
+
+    num_slices_img = data_img.shape[2]
+    slice_index = st.slider("Select slice index", 0, num_slices_img - 1, int(num_slices_img / 2))  # Default to the middle slice
+
+    with col1:
+        st.image(data_img[:,:,slice_index]/np.max(data_img[:,:,:]))
+
+    with col2:
+        st.image(data_segm[:,:,slice_index]/np.max(data_segm[:,:,:]))
+
+    with col3:
+        st.image(data_ts[:,:,slice_index]/np.max(data_ts[:,:,:]))
 
 st.write("end of app")
